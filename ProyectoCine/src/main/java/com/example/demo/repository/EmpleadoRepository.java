95c7045529288936a6c77bea7d1d0b5ea5210370
Alex Jair <alexjair584@gmail.com>
2024-06-24 01:33:51 -0500

    CRUD EMPLEADO

---
+++ ProyectoCine/src/main/java/com/example/demo/repository/EmpleadoRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.*;
+
+@Repository
+public interface EmpleadoRepository extends JpaRepository<Empleado, Integer> {
+	
+	
+	
+
+}
